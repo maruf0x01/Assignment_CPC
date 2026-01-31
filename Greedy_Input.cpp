a09fe68478b768f27780d25bc816ae66070df48b
Maruf Hasan <108041840+maruf0x01@users.noreply.github.com>
2026-01-31 15:04:51 +0000

    Add Greedy_Input.cpp for basic name input and output functionality

---
+++ Greedy_Input.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string firstName, lastName;
+    cin >> firstName >> lastName;
+    cout << "Name: " << firstName << " " << lastName << endl;
+    return 0;
+}
